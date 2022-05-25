0fdd3684571398b1c320fdf0434af8b7c3737da3
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-25 16:40:32 +0600

    ASP.NET Core .net 6 learn Day 3 Learn completed

---
+++ ASP.NetCoreLearn_WebApp/ASP.NetCoreLearn_WebApp/Controllers/CityController.cs
@@ -0,0 +1,121 @@
+﻿using ASP.NetCoreLearn_WebApp.Data;
+using ASP.NetCoreLearn_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NetCoreLearn_WebApp.Controllers
+{
+    public class CityController : Controller
+    {
+        private ApplicationDbContext _Context;
+
+        public CityController(ApplicationDbContext context)
+        {
+            _Context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<City> cities = _Context.Cities;
+            return View(cities);
+        }
+
+        //Add city start
+
+        [HttpGet]
+        public IActionResult AddCity()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddCity(City city)
+        {
+            if (ModelState.IsValid)
+            {
+                _Context.Cities.Add(city);
+                _Context.SaveChanges();
+                TempData["succcess"] = "New city added!";
+                return RedirectToAction("Index");
+            }
+            return View(city);
+        }
+
+
+        //Add city End
+
+
+        //Edit city start
+        [HttpGet]
+        public IActionResult EditCity(int? id)
+        {   
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var cities = _Context.Cities.Find(id);
+            if (cities == null)
+            {
+                return NotFound();
+            }
+
+            return View(cities);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditCity(City city)
+        {
+            if (ModelState.IsValid)
+            {
+                _Context.Cities.Update(city);
+                _Context.SaveChanges();
+
+                TempData["succcess"] = "Updated city Successfuly";
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        //Edit city End
+
+
+
+        //Delete city start
+        [HttpGet]
+        public IActionResult DeleteCity(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var cities = _Context.Cities.Find(id);
+            if (cities == null)
+            {
+                return NotFound();
+            }
+
+            return View(cities);
+        }
+
+
+        [HttpPost, ActionName("DeleteCity")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int? id)
+        {
+            var cities = _Context.Cities.Find(id);
+            if (cities == null)
+            {
+                return NotFound();
+            }
+            _Context.Cities.Remove(cities);
+            _Context.SaveChanges();
+            TempData["succcess"] = "Deleted city Successfuly";
+            return RedirectToAction("Index");
+        }
+
+        //Delete city End
+    }
+}
--- ASP.NetCoreLearn_WebApp/ASP.NetCoreLearn_WebApp/Program.cs
+++ ASP.NetCoreLearn_WebApp/ASP.NetCoreLearn_WebApp/Program.cs
@@ -1,7 +1,15 @@
+using ASP.NetCoreLearn_WebApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultCS"));
+});
 
 var app = builder.Build();
 
