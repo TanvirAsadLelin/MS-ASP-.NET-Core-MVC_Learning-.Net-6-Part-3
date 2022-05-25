cc4c4a9f6b2267a3ebfadde1bdc4fed6b7d0afd0
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-25 09:32:24 +0600

    ASP.NET Core .net 6 learn Day 1 added now

---
+++ ASP.NetCoreLearn_WebApp/ASP.NetCoreLearn_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NetCoreLearn_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
