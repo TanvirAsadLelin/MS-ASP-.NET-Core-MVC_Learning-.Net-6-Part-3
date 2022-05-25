cf1ddbaa60a899add45bbd646a6957bfcf6e9ab9
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-25 14:47:26 +0600

    ASP.NET Core .net 6 learn Day 2 Data and Model files added

---
+++ ASP.NetCoreLearn_WebApp/ASP.NetCoreLearn_WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ASP.NetCoreLearn_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NetCoreLearn_WebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<City> Cities { get; set; }
+    }
+}
---
+++ ASP.NetCoreLearn_WebApp/ASP.NetCoreLearn_WebApp/Models/City.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace ASP.NetCoreLearn_WebApp.Models
+{
+    public class City
+    {   
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Population { get; set; }
+        [DisplayName("Size of Area")]
+        public int AreaSize { get; set; }
+        [DisplayName("Date of Build")]
+        public DateTime BuildDate { get; set; } = DateTime.Now;
+
+    }
+}
