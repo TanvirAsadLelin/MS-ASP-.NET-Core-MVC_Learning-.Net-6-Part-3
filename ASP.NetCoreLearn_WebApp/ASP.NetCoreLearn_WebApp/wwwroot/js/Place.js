80613771decc6c8f9ce883066b94eab5eb9cefde
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-29 12:41:44 +0600

    ASP.NET Core .net 6 learn Day 7 Add Databele from Datatable.net website

---
+++ ASP.NetCoreLearn_WebApp/ASP.NetCoreLearn_WebApp/wwwroot/js/Place.js
@@ -0,0 +1,14 @@
+﻿var dTable;
+$(document).ready(function () {
+    dTable = $('#myTable').DataTable({
+        "ajax": {
+          "url":"/Admin/Place/AllPlaces"
+        },
+            "columns": [
+                { "data": "placeName" },
+                { "data": "placeDescription" },
+                { "data": "placeVisitRate" }
+               
+            ]
+    });
+});
